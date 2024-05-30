de7865b9a470f07bd742ed67d2b938c6857c15d8
Brayan Ccari Lindo <brayancclindo@gmail.com>
2024-05-30 00:43:17 -0500

    Beta Project Smile

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { CampañaGiftSmileType } from "../type/types";
 import { Rocket } from "lucide-react";
-import { useSmileContext } from "../Api/userContext";
 
 function Card({
   campaña,
